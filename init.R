c9e89e62045b18c18af7806316f1ff397a4f83fc
almartin82 <almartin@gmail.com>
2015-07-15 00:40:51 -0400

    initial commit

---
+++ init.R
@@ -0,0 +1,8 @@
+# Example `init.r` file
+
+install.packages("readr", dependencies = TRUE)
+install.packages("dplyr", dependencies = TRUE)
+install.packages("zoo", dependencies = TRUE)
+install.packages("tidyr", dependencies = TRUE)
+install.packages("ggplot2", dependencies = TRUE)
+install.packages("DT", dependencies = TRUE)
