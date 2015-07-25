4e462d82734dff72f17096a18f27f34835f606b7
almartin82 <almartin@gmail.com>
2015-07-25 14:46:09 -0400

    update recipients

--- send_email.R
+++ send_email.R
@@ -3,7 +3,8 @@ library(RMandrill)
 RMandrill::mandrill_send_template(
   api_key = Sys.getenv("MANDRILL_KEY"),
   template_name = 'hpk-times',
-  recipient = 'almartin@gmail.com',
+  recipient = c('almartin@gmail.com', 'czapmike@gmail.com', 'enusbaum@gmail.com', 
+                'mikec116@gmail.com', 'ptbeatty@gmail.com'),
   variables = data.frame(
     'name' = c('s3_link'), 
     'content' = c(paste0('https://s3.amazonaws.com/hpk/', rmd_filename))
