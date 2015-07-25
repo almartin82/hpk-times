459d4a0f19c455ce5fdff45b942311d6d9714317
almartin82 <almartin@gmail.com>
2015-07-25 19:25:15 -0400

    html wasnt generating

--- prog.r
+++ prog.r
@@ -1,13 +1,17 @@
-library(knitr)
-knitr::knit('hpk_daily.Rmd')
+
+#http://stackoverflow.com/a/10969107/561698
+rmd_filename = paste0('hpk_daily_', gsub('[-:]', '_', Sys.Date()), '.html')
+require(knitr) # required for knitting from rmd to md
+require(markdown) # required for md to html 
+knit('hpk_daily.Rmd', 'hpk_daily.md') # creates md file
+markdownToHTML('test.md', rmd_filename) # creates html file
+
 
 library(aws.signature)
 library(aws.s3)
 
-rmd_filename = paste0('hpk_daily_', gsub('[-:]', '_', Sys.Date()), '.html')
-
 aws.s3::putobject(
-  file = 'hpk_daily.html',
+  file = rmd_filename,
   bucket = 'hpk', 
   object = rmd_filename,
   parse_response = FALSE
--- send_email.R
+++ send_email.R
@@ -1,10 +1,19 @@
 library(RMandrill)
+devtools::load_all(pkg = "C:\\Users\\AMartin\\Dropbox\\repositories\\RMandrill")
+
+rmd_filename = paste0('hpk_daily_', gsub('[-:]', '_', Sys.Date()), '.html')
 
 RMandrill::mandrill_send_template(
   api_key = Sys.getenv("MANDRILL_KEY"),
   template_name = 'hpk-times',
-  recipient = c('almartin@gmail.com', 'czapmike@gmail.com', 'enusbaum@gmail.com', 
-                'mikec116@gmail.com', 'ptbeatty@gmail.com'),
+  recipient = 'almartin@gmail.com',
+#   recipient = data.frame(
+#      email = c('almartin@gmail.com', 'czapmike@gmail.com', 'enusbaum@gmail.com', 
+#                 'mikec116@gmail.com', 'ptbeatty@gmail.com', 'zoheri@gmail.com'),
+#      name = c('alm', 'czap', 'eric', 'card', 'ptb', 'omar'),
+#      type = c('to', 'cc', 'cc', 'cc', 'cc', 'cc'),
+#      stringsAsFactors = FALSE
+#     ),
   variables = data.frame(
     'name' = c('s3_link'), 
     'content' = c(paste0('https://s3.amazonaws.com/hpk/', rmd_filename))
