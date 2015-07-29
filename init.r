93194f6b7c5f8b964352c22f3286805d4a389297
almartin82 <almartin@gmail.com>
2015-07-28 23:02:27 -0400

    dont reinstall

--- init.r
+++ init.r
@@ -1,16 +1,18 @@
-install.packages("rmarkdown")
-install.packages("DT")
-install.packages("ggplot2")
-install.packages("readr")
-install.packages("dplyr")
-install.packages("zoo")
-install.packages("tidyr")
-install.packages("magrittr")
-install.packages("fitdistrplus")
-install.packages("httr")
-install.packages("XML")
-install.packages("devtools")
-install.packages("knitr")
+
+
+my_packages = c("rmarkdown", "DT", "ggplot2", "readr", "dplyr", "zoo", "tidyr", "magrittr", "fitdistrplus", "httr", "XML", "devtools", "knitr")
+
+				install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p, dependencies = TRUE)
+  }
+  else {
+    cat(paste("Skipping already installed package:", p, "\n"))
+  }
+}
+invisible(sapply(my_packages, install_if_missing))
+
+# github packages
 devtools::install_github('cloudyr/aws.signature')
 devtools::install_github('cloudyr/aws.s3')
 devtools::install_github("almartin82/RMandrill", ref = 'almartin')
